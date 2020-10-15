e5e5d263d42b0477d5314b053f99fe14aa6842d5
wilmer <wil.quexel@gmail.com>
2020-10-14 21:03:26 -0600

    login grupo 7

---
+++ WebApplicationLogin/Vistas/login/Login.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="WebApplicationLogin.Vistas.login.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">  
+        
+            <div class="imgcontainer">
+                <img src="../../Imagenes/img.jpg" alt="img" class="img" /> 
+            </div>
+
+            <div class="container">
+                <label for="uname"><b>Usuario</b></label>
+                <asp:TextBox CssClass="txtUsername" ID="txtUsername" runat="server" type="text" placeholder="Ingrese el nombre de usuario" name="uname" required=""></asp:TextBox>
+               
+                <label for="psw"><b>Contraseña</b></label>
+                <asp:TextBox CssClass="txtpassword" ID="txtpassword" runat="server" type="password" placeholder="Ingrese Contraseña" name="psw" required=""></asp:TextBox>
+                               
+                <asp:Button class="btnAceptar" ID="btnAceptar" runat="server" Text="Ingresar" type="submit" />
+            </div>
+
+
+            <div class="container" style="background-color:#f1f1f1">
+                <asp:Button ID="btnCancelar" runat="server" Text="Cancelar" type="button" class="cancelbtn" />
+                
+                <span class="psw">Olvide mi <a href="#">Contraseña?</a></span>
+            </div>       
+    </form>
+</body>
+</html>
---
+++ WebApplicationLogin/Vistas/login/Login.aspx.designer.cs
@@ -0,0 +1,62 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace WebApplicationLogin.Vistas.login
+{
+
+
+    public partial class Login
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control txtUsername.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtUsername;
+
+        /// <summary>
+        /// Control txtpassword.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtpassword;
+
+        /// <summary>
+        /// Control btnAceptar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAceptar;
+
+        /// <summary>
+        /// Control btnCancelar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnCancelar;
+    }
+}
